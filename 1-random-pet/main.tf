36511d5540f07cad18473e8174a0f54a361720b4
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-27 22:15:52 -0500

    dia 5

---
+++ 1-random-pet/main.tf
@@ -0,0 +1,6 @@
+resource random_pet name {
+  length    = var.length
+  prefix    = var.prefix
+  separator = var.separator
+
+}
---
+++ 1-random-pet/variables.tf
@@ -0,0 +1,19 @@
+variable prefix {
+  type        = string
+  default     = "Hola"
+  description = "description"
+}
+
+variable length {
+  type        = number
+  default     = 1
+  description = "description"
+}
+
+
+variable separator {
+  type        = string
+  default     = " "
+  description = "description"
+}
+
