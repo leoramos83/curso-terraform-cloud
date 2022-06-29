fd55306184a58e071bfd3640f4b15f88e7d429b2
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-29 02:30:13 -0500

    dia 6

--- 3-aws-aim-policy/variables.tf
+++ 3-aws-aim-policy/variables.tf
@@ -1,10 +1,19 @@
 variable access_key {
   type        = string
-description = "access_key"
+  description = "access_key"
+  sensitive = true
 }
 
 variable secret_key {
   type        = string
   description = "secret_key"
+  sensitive = true
 }
+
+variable region {
+  type        = string
+  description = "region"
+  
+}
+
 
