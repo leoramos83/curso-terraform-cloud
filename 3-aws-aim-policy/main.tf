577e4742bb38943a529e6f0d8bf372b3f7d43a12
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-29 01:54:58 -0500

    dia 6

---
+++ 3-aws-aim-policy/main.tf
@@ -0,0 +1,29 @@
+
+# remove this part
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_iam_user" "admin-user" {
+  name = "carly"
+  
+  tags = {
+  Description = "Jefe Tecnico"
+  }
+}
+
+resource "aws_iam_policy" "adminUser" {
+  name = "AdminUsers"
+  policy =  file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "carly-admin-access" {
+  
+  user = aws_iam_user.admin-user.name
+
+  policy_arn = aws_iam_policy.adminUser.arn
+
+
+}
---
+++ 3-aws-aim-policy/variables.tf
@@ -0,0 +1,10 @@
+variable access_key {
+  type        = string
+description = "access_key"
+}
+
+variable secret_key {
+  type        = string
+  description = "secret_key"
+}
+
